39f9d13b0105e6a0c4bdcf406e0e232810938d98
Filip Bozic <filip.bozic@axilis.com>
2020-10-12 17:22:02 +0200

    implement patch and push jobs

---
+++ src/scripts/git-commit-push.sh
@@ -0,0 +1,10 @@
+git add $INFRA_APP_PATH
+git commit -F- <<EOF
+chore(release): Release $IMAGE_NAME $IMAGE_TAG
+Automated version update from CI
+    repository: $CIRCLE_PROJECT_REPONAME
+    user: $CIRCLE_USERNAME
+    commit: $CIRCLE_SHA1
+    build number: $CIRCLE_BUILD_NUM
+EOF
+git push origin master
---
+++ src/scripts/patch-apply.sh
@@ -0,0 +1,2 @@
+kubectl patch --dry-run --local -o yaml -f $INFRA_APP_PATH -p "$(cat patch.yml)" > deployment
+mv deployment $INFRA_APP_PATH
