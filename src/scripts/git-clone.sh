48cb5dcc4476e0ca49ffb08578d3bed26173add2
Filip Bozic <filip.bozic@axilis.com>
2020-10-12 17:29:39 +0200

    double quote to prevent globbing and word splitting

--- src/scripts/git-clone.sh
+++ src/scripts/git-clone.sh
@@ -1,3 +1,3 @@
-git clone git@github.com:$INFRA_GIT_ACCOUNT/$INFRA_GIT_REPOSITORY.git .
+git clone git@github.com:"$INFRA_GIT_ACCOUNT"/"$INFRA_GIT_REPOSITORY".git .
 git config user.name "$GIT_USER_NAME"
 git config user.email "$GIT_USER_EMAIL"
