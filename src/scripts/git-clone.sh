43dbe787674c3b4fec90c673bacc867b809135ad
Filip Bozic <filip.bozic@axilis.com>
2020-10-13 14:38:30 +0200

    double quote to prevent globbing and word splitting

--- src/scripts/git-clone.sh
+++ src/scripts/git-clone.sh
@@ -1,6 +1,6 @@
 echo "$GIT_INFRA_ACCOUNT"
-echo ${GIT_INFRA_ACCOUNT}
-echo ${!GIT_INFRA_ACCOUNT}
+echo "${GIT_INFRA_ACCOUNT}"
+echo "${!GIT_INFRA_ACCOUNT}"
 
 git clone git@github.com:"$GIT_INFRA_ACCOUNT"/"$GIT_INFRA_REPO".git .
 git config user.name "$GIT_USER_NAME"
