903a9120255e4c83671a94fb5ce4e7a8dbc3eec3
Filip Bozic <filip.bozic@axilis.com>
2020-10-13 18:13:18 +0200

    test git clone

--- src/scripts/git-clone.sh
+++ src/scripts/git-clone.sh
@@ -5,6 +5,7 @@ echo "${CIRCLE_PROJECT_USERNAME}"
 something=CIRCLE_PROJECT_USERNAME
 echo "${!something}"
 
-git clone git@github.com:"${!GIT_INFRA_ACCOUNT}"/"${GIT_INFRA_REPO}".git .
+git clone git@github.com:superbet-group/dummy.infrastructure.git .
+# git clone git@github.com:"${!GIT_INFRA_ACCOUNT}"/"${GIT_INFRA_REPO}".git .
 git config user.name "${GIT_USER_NAME}"
 git config user.email "${GIT_USER_EMAIL}"
