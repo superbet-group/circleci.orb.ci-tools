718e84e3c6034e49ee85a41cb1406772f86698fc
Filip Bozic <filip.bozic@axilis.com>
2020-10-13 13:29:40 +0200

    print env vars for debugging

--- src/scripts/git-clone.sh
+++ src/scripts/git-clone.sh
@@ -1,3 +1,6 @@
-git clone git@github.com:"$INFRA_GIT_ACCOUNT"/"$GIT_INFRA_REPO".git .
+echo "git account"
+echo $GIT_INFRA_ACCOUNT
+
+git clone git@github.com:"$GIT_INFRA_ACCOUNT"/"$GIT_INFRA_REPO".git .
 git config user.name "$GIT_USER_NAME"
 git config user.email "$GIT_USER_EMAIL"
