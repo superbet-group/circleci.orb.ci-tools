65754d65803732dbfbed1438a73b21faa9465f09
Filip Bozic <filip.bozic@axilis.com>
2020-10-13 18:37:51 +0200

    Revert "use GH token for git clone"
    This reverts commit 77ecc0806c681a5b032e9183cd77fba29fa10d90.

--- src/scripts/git-clone.sh
+++ src/scripts/git-clone.sh
@@ -1,3 +1,11 @@
-git clone https://"${GH_TOKEN}":x-oauth-basic@github.com:"${!GIT_INFRA_ACCOUNT}"/"${GIT_INFRA_REPO}".git .
+echo "${!GIT_INFRA_ACCOUNT}"
+# echo "${GIT_INFRA_ACCOUNT}"
+# echo "${!GIT_INFRA_ACCOUNT}"
+echo "${CIRCLE_PROJECT_USERNAME}"
+something=CIRCLE_PROJECT_USERNAME
+echo "${!something}"
+
+git clone git@github.com:superbet-group/dummy.infrastructure.git .
+# git clone git@github.com:"${!GIT_INFRA_ACCOUNT}"/"${GIT_INFRA_REPO}".git .
 git config user.name "${GIT_USER_NAME}"
 git config user.email "${GIT_USER_EMAIL}"
