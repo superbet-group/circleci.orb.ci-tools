9d4c242c3a25d76f68db82cce3c5699335e648b3
Filip Bozic <filip.bozic@axilis.com>
2020-10-13 15:40:11 +0200

    test variable expansion

--- src/scripts/git-clone.sh
+++ src/scripts/git-clone.sh
@@ -2,6 +2,8 @@ echo "$GIT_INFRA_ACCOUNT"
 # echo "${GIT_INFRA_ACCOUNT}"
 # echo "${!GIT_INFRA_ACCOUNT}"
 echo "${CIRCLE_PROJECT_USERNAME}"
+something=CIRCLE_PROJECT_USERNAME
+echo "${!something}"
 
 git clone git@github.com:"$GIT_INFRA_ACCOUNT"/"$GIT_INFRA_REPO".git .
 git config user.name "$GIT_USER_NAME"
